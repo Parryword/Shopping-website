7ffbc899069ac5dcd139a9141f099f599be7288e
Parryword <yigit.efe@std.ieu.edu.tr>
2023-06-15 20:19:30 +0300

    created about and contact pages

---
+++ src/about.php
@@ -0,0 +1,23 @@
+<?php
+	include("top.php");
+	?>
+	
+	<style>
+		#a{
+			max-width: 80%;
+			margin-top: 1em;
+			margin-bottom: 1em;
+			padding-top: 1em;
+			padding-bottom: 1em;
+			background-color: #e4e4e4;
+		}
+	</style>
+	<div id="a" class="container-fluid">
+		<h3>About</h3>
+		<div class="text-justify">
+			Welcome to Foodie, the online marketplace for fresh and delicious food. Whether you're looking for fruits, vegetables, meat, dairy, bakery, or ready-made meals, we have it all. You can browse our wide selection of products, compare prices and ratings, and order online with just a few clicks. We deliver to your door in less than an hour, or you can pick up your order at a nearby store. You can also save money with our weekly deals and coupons, and earn rewards with our loyalty program. Foodie is more than just a food shopping website. It's a community of food lovers who share recipes, tips, and reviews. You can join our forums, follow our blog, or subscribe to our newsletter to get the latest news and updates. You can also follow us on social media to see what we're cooking up. Foodie is your one-stop shop for all your food needs. Try us today and see why we're the best choice for online food shopping
+		</div>
+	</div>
+<?php
+	include("bottom.php");
+?>
---
+++ src/contact.php
@@ -0,0 +1,49 @@
+<?php
+	include("top.php");
+	?>
+	<style>
+		#a{
+			max-width: 80%;
+			margin-top: 1em;
+			margin-bottom: 1em;
+			padding-top: 1em;
+			padding-bottom: 1em;
+			background-color: #e4e4e4;
+		}
+		.col-md-6{
+			margin-top: 1em;
+			min-height: 100px;
+		}
+	</style>
+
+	<div id="a" class="container-fluid text-justify">
+		<h3 class="text-center">Contact</h3>
+		<div>
+			We love hearing from you. If you have any questions, comments, or feedback, please feel free to contact us using any of the methods below. We'll get back to you as soon as possible.
+		</div>
+		<div class="col-md-6">
+			<h4>Phone</h4>You can call us toll-free at 1-800-FOODIE (1-800-366-343) from Monday to Friday, 9 am to 5 pm EST.
+		</div>
+		<div class="col-md-6">
+			<h4>Email</h4>You can email us at support@foodie.com anytime. We'll respond within 24 hours.
+		</div>
+			<div class="col-md-6">
+			<h4>Chat</h4>You can chat with us online by clicking on the chat icon at the bottom right corner of our website. Our chat agents are availablefrom Monday to Friday, 9 am to 5 pm EST.
+		</div>
+	
+		<div class="col-md-6">
+			<h4>Social Media</h4>You can also reach out to us on social media. Follow us on Facebook, Twitter, Instagram, and Pinterest to get the latest updates and join the conversation.
+		</div>
+		
+		<div class="col-md-6">
+			<h4>Mail</h4>If you prefer to write to us, you can send us a letter at:
+			<br>Foodie Inc.
+			<br>123 Main Street
+			<br>New York, NY 10001
+			<br>USA
+		</div>
+
+	</div>
+<?php
+	include("bottom.php");
+?>
